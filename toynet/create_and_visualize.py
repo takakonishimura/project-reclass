24503d10b95eeebdbe06bcd54ef64bb0975e5345
Tay Nishimura <takakonishimura@gmail.com>
2020-05-20 05:01:15 +0000

    restructure commandline input processing

--- toynet/create_and_visualize.py
+++ toynet/create_and_visualize.py
@@ -1,13 +1,25 @@
+import sys
 from toynet.toynet import ToyNet
-from toynet.toytopo import ToyTopo
 
-def run():
-    "Create Mininet & Visualize it"
+def run(filepath:str, visualize:bool=True, interact:bool=False, title:str='Toy Network'):
+    """Takes an XML file and either visualizes or interacts with a network simulation
+        depending on the flags passed in.
+        Both use the ToyNet object's ToyTopoConfig as input.
+        run script currently defaults to visualization but no iteraction if no flags set.
+    """
+    toynet = ToyNet(filepath)
+    if visualize:
+        print('TOYNET LOG---- creating new image representation')
+        imageFileName = toynet.visualize(title)
+        print('TOYNET LOG---- image created, see: ' + imageFileName)
+    if interact:
+        print('TOYNET LOG---- creating interactive simluation')
+        toynet.interact()
+        print('TOYNET LOG---- closing simulation')
 
-    toynet = ToyNet(topology=ToyTopo()) # ToyNet( topo=TreeTopo( depth=2, fanout=6 ) )
+if __name__ == '__main__':
+    filepath = sys.argv[1]
+    visualize = sys.argv[2].upper() == "TRUE"
+    interact = sys.argv[3].upper() == "TRUE"
 
-    toynet.visualize()
-    #toynet.interact()
-
-if __name__== "__main__":
-  run() 
+    run(filepath, visualize, interact)
--- toynet/run_toynet.sh
+++ toynet/run_toynet.sh
@@ -1,9 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+FILEDIR="sample_inputs/"
+FILENAME="sample.xml"
+VISUALIZE="FALSE"
+INTERACT="FALSE"
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -f|--filename)
+    FILENAME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -v|--visualize)
+    VISUALIZE="TRUE"
+    shift # past argument
+    ;;
+    -i|--interact)
+    INTERACT="TRUE"
+    shift # past argument
+    ;;
+esac
+done
 
 echo 'TOYNET LOG---- cleaning any previous runs'
 sudo python3 clean_toynet.py
 
 echo 'TOYNET LOG---- creating new toynet'
-sudo python3 create_and_visualize.py
 
-echo 'TOYNET LOG---- done processing, see toy_network.png'
+if [[ "FALSE" == "$VISUALIZE" ]] && [[ "FALSE" == "$INTERACT" ]];
+then
+    echo '__INFO___ Defaulting to just Visualization'
+    VISUALIZE="TRUE"
+    INTERACT="FALSE"
+fi
+sudo python3 create_and_visualize.py ${FILEDIR}${FILENAME} ${TITLE} ${VISUALIZE} ${INTERACT}
+
+echo 'TOYNET LOG---- done processing'
